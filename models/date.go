07335e06cec42ee5d734303c91ff7fcb3c779100
Buğra Çalışkandinç <bugracaliskandinc@gmail.com>
2026-04-04 21:08:45 +0300

    Update date models with additional attributes and json tags

--- models/date.go
+++ models/date.go
@@ -3,20 +3,31 @@ package models
 import (
 	"errors"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type TuDate struct {
-	Date time.Time
+	UUID string    `json:"id"`
+	Name string    `json:"name"`
+	Date time.Time `json:"date"`
 }
 
 func NewTuDateFromString(dateStr string) (*TuDate, error) {
-	var tmp TuDate
-
-	if len(dateStr) < 19 {
-		return nil, errors.New("Date string too short")
+	if len(dateStr) > 19 {
+		return nil, errors.New("Error: Date string too long")
 	}
 
-	if parsedDate, err := time.Parse("2006-01-02 15:04:05", dateStr[0:19]); err != nil {
+	var tmp TuDate
+	tmp.UUID = uuid.New().String()
+
+	layoutStr := "2006-01-02 15:04:05"
+	if len(dateStr) < 19 {
+		layoutStr = "2006-01-02"
+	}
+
+	parsedDate, err := time.Parse(layoutStr, dateStr)
+	if err != nil {
 		return nil, err
 	} else {
 		tmp.Date = parsedDate
@@ -27,7 +38,25 @@ func NewTuDateFromString(dateStr string) (*TuDate, error) {
 
 func NewTuDateFromDate(date time.Time) *TuDate {
 	var tmp TuDate
+	tmp.UUID = uuid.NewString()
 	tmp.Date = date
 
 	return &tmp
+}
+
+func NewTuDateFromStringWithName(dateStr string, name string) (*TuDate, error) {
+	if tmp, err := NewTuDateFromString(dateStr); err == nil {
+		tmp.Name = name
+		return tmp, nil
+	} else {
+		return nil, err
+	}
+
+}
+
+func NewTuDateFromDateWithName(date time.Time, name string) *TuDate {
+	tmp := NewTuDateFromDate(date)
+	tmp.Name = name
+
+	return tmp
 }
