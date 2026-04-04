668518561b73c2b1b0a8b3aa9372c1d99aaef55f
Buğra Çalışkandinç <bugracaliskandinc@gmail.com>
2026-04-04 21:06:49 +0300

    Update main.go with initilaziation logic

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "tu/cli"
+import (
+	"time"
+	"tu/cli"
+	"tu/db"
+	"tu/models"
+)
+
+func addNewDates(db db.Storage) {
+	tudate1 := models.NewTuDateFromDate(time.Now())
+	db.Add(*tudate1)
+	tudate2 := models.NewTuDateFromDate(time.Now())
+	db.Add(*tudate2)
+}
 
 func main() {
-	cli.Execute()
+	jsonFileDirPath := "data/"
+	jsonFilePath := jsonFileDirPath + "data.db"
+	TuDateStorage := db.NewStorage(jsonFilePath)
+	err := TuDateStorage.Load()
+	if err != nil {
+		panic("Error: Cannot read storage: " + err.Error())
+	}
+
+	//addNewDates(*TuDateStorage)
+
+	cli.Execute(TuDateStorage)
 }
